56735e1a941f5b9bd4c9924ba017a4d991b7633c
Sebastian Kim <sebastian_jr@yeah.net>
2019-09-09 20:29:50 -0400

    lab0 finished

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+if __name__ == '__main__':
+    r_num1 = random.randint(0, 100)
+    r_num2 = random.randint(0, 100)
+    print("Rong Jin", r_num1, r_num2, 
+          f"Sum = {r_num1+r_num2}", 
+          f"Average = {(r_num1+r_num2)/2}", 
+          sep='\n')
+    
